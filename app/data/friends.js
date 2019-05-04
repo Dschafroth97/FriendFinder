d4b796c6fbfde0b64fd049cc40ad8fa466a13c1c
Donovan Schafroth <donovanschafroth@gmail.com>
2019-05-04 11:22:30 -0500

    html pages done, need routes and javascript logic

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+    {
+        "name": "Robert Downey Jr.",
+        "photo": "https://cbsnews1.cbsistatic.com/hub/i/2017/06/29/fe40ccf2-90d6-4b24-92bf-b8795d04c2c2/gettyimages-803015182.jpg",
+        "scores": [5, 1, 5, 3, 1, 1, 2, 2, 5, 5]
+    },
+    {
+        "name": "Donovan Schafroth",
+        "photo": "",
+        "scores": [1, 5, 5, 5, 3, 5, 5, 5, 2, 4]
+    }
+];
+
+module.exports = friends;
