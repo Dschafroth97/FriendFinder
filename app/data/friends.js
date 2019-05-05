847860e25848653f324ab25982ae33d1e52d4ab7
Donovan Schafroth <donovanschafroth@gmail.com>
2019-05-05 09:16:57 -0500

    functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,9 +5,14 @@ var friends = [
         "scores": [5, 1, 5, 3, 1, 1, 2, 2, 5, 5]
     },
     {
-        "name": "Donovan Schafroth",
-        "photo": "",
+        "name": "Daniel Woods",
+        "photo": "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/28279440_1802335999790783_8237019859980467949_n.jpg?_nc_cat=107&_nc_ht=scontent-dfw5-2.xx&oh=8bc5b0907b802a429f8faf472b60252b&oe=5D7301AA",
         "scores": [1, 5, 5, 5, 3, 5, 5, 5, 2, 4]
+    },
+    {
+        "name": "Chris Sharma",
+        "photo": "https://www.planetmountain.com/img/2/7685.jpg",
+        "scores": [3, 5, 3, 4, 5, 1, 5, 2, 1, 5]
     }
 ];
 
